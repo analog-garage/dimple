62875d29fea9016e80563d8f4f510dac4ee0b734
Shawn Hershey <shawn.hershey@analog.com>
2012-10-09 17:16:54 -0400

    added demo test

--- demo/01_6BitCode/run.m
+++ demo/01_6BitCode/run.m
@@ -14,4 +14,35 @@
 %   limitations under the License.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-example
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Define 4 bit xor from two 3 bit xors
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+b = Bit(4,1);
+%v = newVariable(4,1);
+%setDomain(v,[1 0]);
+
+XorGraph = FactorGraph(b);
+c = Bit;
+XorGraph.addFactor(@xorDelta_01_6BitCode,b(1),b(2),c);
+XorGraph.addFactor(@xorDelta_01_6BitCode,b(3),b(4),c);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Create graph for 6 bit code
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    
+d = Bit(6,1);
+MyGraph = FactorGraph(d);
+MyGraph.addFactor(XorGraph,d([1:3 5]));
+MyGraph.addFactor(XorGraph,d([1 2 4 6]));
+    
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Set priors and Solve
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+priors =  [.75 .6 .9 .1 .2 .9]';
+d.Input = priors;
+MyGraph.Solver.setNumIterations(20);
+MyGraph.solve();
+disp((d.Belief>.5)');
+d.Belief
---
+++ modelers/matlab/tests/algoDemos/testDemos.m
@@ -0,0 +1,46 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Copyright 2012 Analog Devices, Inc.
+%
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function testDemos()
+
+    clear all;
+    demo_dir = '../../../../demo';
+    my_current_directory = pwd;
+    cd(demo_dir);
+
+    %% 00_XorAndNot
+
+    cd('00_XorAndNot');
+    run;
+    close;
+
+    assertElementsAlmostEqual(w.Belief,0.375);
+    assertElementsAlmostEqual(x.Belief,0.6250);
+    assertElementsAlmostEqual(y.Belief,0.75);
+    assertElementsAlmostEqual(z.Belief,0.75);
+
+    %% 01_6BitCode
+
+    cd(my_current_directory);
+
+    load 6bitgolden;
+    cd(demo_dir);
+    cd('01_6BitCode');
+    run;
+    assertElementsAlmostEqual(d.Belief,x);
+
+    cd(my_current_directory);
+end
