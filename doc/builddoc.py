54613f0b1e6c445cd3be3c73541e47962e0bc891
Shawn Hershey <shawn.hershey@analog.com>
2012-11-27 15:07:54 -0500

    build doc fixed

--- doc/builddoc.py
+++ doc/builddoc.py
@@ -19,74 +19,27 @@ import os
 import shutil
 import glob
 
-def a2x(outputDir,type,fileName):
-    #TODO: make sure a2x exists
-    p = os.getcwd()
-    shutil.copyfile(fileName,outputDir + "/" + fileName)
-    os.chdir(outputDir)
-    os.system("a2x -f " + type + " " + fileName)
-    os.chdir(p)
-
-def a2xpdf(fileName,outputDir):
-    a2x(outputDir,"pdf",fileName + ".txt")
-    shutil.copyfile(outputDir + "/" + fileName + ".pdf",fileName + ".pdf")
-
-def a2xhtml(fileName,outputDir):
-    a2x(outputDir,"xhtml",fileName + ".txt")
-
-def asciidochtml(fileName,outputDir,singlePage=False):
-    p = os.getcwd()
-    shutil.copyfile(fileName + ".txt",outputDir + "/" + fileName+".txt")
-    os.chdir(outputDir)
-    if singlePage:
-        os.system('asciidoc -a data-uri ' + fileName + '.txt')
-    else:
-        os.system('asciidoc -b xhtml11 ' + fileName + '.txt')
-    os.chdir(p)
-
-def producedoc2(fileName,option):
-    os.system('pdflatex ' + fileName)
-
 def producedoc(fileName,option):
-    if option.html_doc:
-        if option.pretty_doc:
-            a2xhtml(fileName,option.output_dir)
-        else:
-            asciidochtml(fileName,option.output_dir,option.single_page)
-    if option.pdf:
-        a2xpdf(fileName,option.output_dir)
+    os.system('pdflatex ' + fileName)
 
 if __name__ == "__main__":
     
     parser = optparse.OptionParser()
     parser.add_option("-u","--user_doc",action="store_true",default=False,dest="user_doc")
-    parser.add_option("-n","--quick_doc",action="store_true",default=False,dest="pretty_doc")
     parser.add_option("-d","--devel_doc",action="store_true",default=False,dest="devel_doc")
-    parser.add_option("-x","--html_doc",action="store_true",default=False,dest="html_doc")
-    parser.add_option("-p","--pdf",action="store_true",default=False,dest="pdf")
     parser.add_option("-f","--user_doc_filename",dest="user_doc_filename",default="DimpleUserDocumentation")
     parser.add_option("-r","--devel_doc_filename",dest="devel_doc_filename",default="DimpleDeveloperDocumentation")
-    parser.add_option("-o","--output_dir",dest="output_dir",default="output")
     parser.add_option("-c","--clean",dest="clean",default=False,action="store_true")
-    parser.add_option("-s","--single_page",dest="single_page",default=False,action="store_true")
                       
     (option,args) = parser.parse_args()
 
     if option.clean:
-        if os.path.isdir(option.output_dir):
-            shutil.rmtree(option.output_dir)
         for n in glob.glob("*.pdf"):
             os.remove(n)
  
-    if option.user_doc or option.devel_doc:
-        if not os.path.isdir(option.output_dir):
-            os.mkdir(option.output_dir)
-        if os.path.isdir(option.output_dir + "/images"):
-            shutil.rmtree(option.output_dir + "/images")
-        shutil.copytree("images",option.output_dir + "/images")
                      
     #Create user doc
     if option.user_doc:
-        producedoc2(option.user_doc_filename,option)
+        producedoc(option.user_doc_filename,option)
     if option.devel_doc:
-        producedoc2(option.devel_doc_filename,option)
+        producedoc(option.devel_doc_filename,option)
