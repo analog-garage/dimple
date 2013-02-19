fe14107d59784feb9632f249e215ad9977894b29
Shawn Hershey <shawn.hershey@analog.com>
2013-02-19 16:29:53 -0500

    fixed test

--- modelers/matlab/tests/algoRolledUpGraphs/testVectorized.m
+++ modelers/matlab/tests/algoRolledUpGraphs/testVectorized.m
@@ -13,84 +13,85 @@
 %   See the License for the specific language governing permissions and
 %   limitations under the License.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function testVectorized()
 
-setSolver('sumproduct');
+    setSolver('sumproduct');
 
-%% Test basic vectorized discrete
-for version = 1:3
+    %% Test basic vectorized discrete
+    for version = 1:3
 
-    %100 time steps
-    N = 100;
+        %100 time steps
+        N = 100;
 
-    %Now
-    d1 = Discrete(0:1,3,3);
-    %Future
-    d2 = Discrete(0:1,3,3);
+        %Now
+        d1 = Discrete(0:1,3,3);
+        %Future
+        d2 = Discrete(0:1,3,3);
 
-    %Create graph
-    ng = FactorGraph(d1,d2);
+        %Create graph
+        ng = FactorGraph(d1,d2);
 
-    %Create checkerboard constraints.  This is a grid that enforces that all
-    %neighbors must be different.
-    ng.addFactorVectorized(@(a,b) a ~= b, d1(1:end-1,:),d1(2:end,:));
-    ng.addFactorVectorized(@(a,b) a ~= b, d1(:,1:end-1),d2(:,2:end));
+        %Create checkerboard constraints.  This is a grid that enforces that all
+        %neighbors must be different.
+        ng.addFactorVectorized(@(a,b) a ~= b, d1(1:end-1,:),d1(2:end,:));
+        ng.addFactorVectorized(@(a,b) a ~= b, d1(:,1:end-1),d2(:,2:end));
 
-    %Each time step should have the same values.
-    ng.addFactorVectorized(@(a,b) a == b, d1, d2);
+        %Each time step should have the same values.
+        ng.addFactorVectorized(@(a,b) a == b, d1, d2);
 
-    %Create the streaming graph.
-    fg = FactorGraph();
-    dd = DiscreteStream(0:1,3,3);
-    fg.addFactor(ng,dd,dd.getSlice(2));
+        %Create the streaming graph.
+        fg = FactorGraph();
+        dd = DiscreteStream(0:1,3,3);
+        fg.addFactor(ng,dd,dd.getSlice(2));
 
-    %Create the inputs
-    sinput0 = [0.4 0.6 0.4; ...
-              0.6 0.4 0.6; ...
-              0.4 0.6 0.4];
-    sinput1 = 1-sinput0;
+        %Create the inputs
+        sinput0 = [0.4 0.6 0.4; ...
+                  0.6 0.4 0.6; ...
+                  0.4 0.6 0.4];
+        sinput1 = 1-sinput0;
 
-    sinput = zeros(3,3,2);
-    sinput(:,:,1) = sinput0;
-    sinput(:,:,2) = sinput1;
+        sinput = zeros(3,3,2);
+        sinput(:,:,1) = sinput0;
+        sinput(:,:,2) = sinput1;
 
-    input = zeros(3,3,2,N);
-    for i = 1:N
-        input(:,:,:,i) = sinput;
-    end
-
-    %Try three different ways of setting the input
-    if version == 1
-        dsrc = DoubleArrayDataSource([3 3],input);
-    elseif version == 2
-
-        dsrc = DoubleArrayDataSource([3 3]);
+        input = zeros(3,3,2,N);
         for i = 1:N
-            dsrc.add(sinput);
+            input(:,:,:,i) = sinput;
         end
-    else
-        dsrc = DoubleArrayDataSource([3 3]);
-        dsrc.add(input);
+
+        %Try three different ways of setting the input
+        if version == 1
+            dsrc = DoubleArrayDataSource([3 3],input);
+        elseif version == 2
+
+            dsrc = DoubleArrayDataSource([3 3]);
+            for i = 1:N
+                dsrc.add(sinput);
+            end
+        else
+            dsrc = DoubleArrayDataSource([3 3]);
+            dsrc.add(input);
+        end
+
+        dsink = DoubleArrayDataSink([3 3]);
+
+        %Set the data source and data sink
+        dd.DataSource = dsrc;
+        dd.DataSink = dsink;
+
+        %Solve the graph
+        fg.solve();
+
+        %Get all the results
+        while dsink.hasNext()
+           tmp = dsink.getNext();
+        end
+
+        %Look at the last result
+        expected = sinput1 > 0.5;
+        diff = expected-tmp(:,:,2);
+        assertTrue(norm(diff) < 1e-50);
     end
-
-    dsink = DoubleArrayDataSink([3 3]);
-
-    %Set the data source and data sink
-    dd.DataSource = dsrc;
-    dd.DataSink = dsink;
-
-    %Solve the graph
-    fg.solve();
-
-    %Get all the results
-    while dsink.hasNext()
-       tmp = dsink.getNext();
-    end
-
-    %Look at the last result
-    expected = sinput1 > 0.5;
-    diff = expected-tmp(:,:,2);
-    assertTrue(norm(diff) < 1e-100);
 end
-
 
 
