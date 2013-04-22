eec1295829871545fbe0f1234f5347e4f96e4819
jbernst2 <jeff.bernstein@analog.com>
2013-04-22 19:29:39 -0400

    Helper functions to simplify the syntax for built-in factors that represent sampling from standard distributions
    Only minimal testing so far, and not yet in documentation

---
+++ modelers/matlab/factorfunctions/InverseGamma.m
@@ -0,0 +1,40 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Copyright 2013 Analog Devices, Inc.
+%
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function var = InverseGamma(alpha, beta, varargin)
+
+fg = getFactorGraph();            % By default, use the current factor graph
+outSize = {1};                    % By default, result is a single variable
+
+% Parse optional arguments
+for arg=varargin
+    a = arg{1};
+    if (isa(a, 'FactorGraph'))
+        fg = a;                   % Optional argument to specify the factor graph
+    elseif (isnumeric(a) && all(round(a)==a))
+        outSize = num2cell(a);    % Optional argument to specify output variable dimensions
+    end
+end
+
+var = Real(outSize{:});
+
+if (isa(alpha,'VariableBase') || isa(beta,'VariableBase'))
+    fg.addFactor('InverseGamma', alpha, beta, var);     % At least one variable parameter
+else
+    fg.addFactor({'InverseGamma', alpha, beta}, var);   % Constant parameters (more efficient to fix in constructor)
+end
+
+end
