b100a9a24510628ad1062024d9a84bade0d62e28
Christopher Barber <Christopher.Barber@analog.com>
2013-07-01 20:38:19 -0400

    Added ParameterList3 & 4

---
+++ solvers/java/src/main/java/com/analog/lyric/dimple/parameters/ParameterList3.java
@@ -0,0 +1,103 @@
+package com.analog.lyric.dimple.parameters;
+
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public abstract class ParameterList3<Key extends IParameterKey> extends SmallParameterListBase<Key>
+{
+	/*--------
+	 * State
+	 */
+	
+	private static final long serialVersionUID = 1L;
+	
+	protected volatile ParameterValue _parameter0;
+	protected volatile ParameterValue _parameter1;
+	protected volatile ParameterValue _parameter2;
+	
+	/*--------------
+	 * Construction
+	 */
+	
+	protected ParameterList3()
+	{
+		this(Double.NaN, Double.NaN, Double.NaN);
+	}
+	
+	protected ParameterList3(double value0, double value1, double value2)
+	{
+		super(false);
+		_parameter0 = new ParameterValue(value0);
+		_parameter1 = new ParameterValue(value1);
+		_parameter2 = new ParameterValue(value2);
+	}
+	
+	protected ParameterList3(SharedParameterValue value0, SharedParameterValue value1, SharedParameterValue value2)
+	{
+		super(false);
+		_parameter0 = value0;
+		_parameter1 = value1;
+		_parameter2 = value2;
+	}
+	
+	protected ParameterList3(ParameterList3<Key> that)
+	{
+		super(that);
+		_parameter0 = that._parameter0.cloneOrShare();
+		_parameter1 = that._parameter1.cloneOrShare();
+		_parameter2 = that._parameter2.cloneOrShare();
+	}
+	
+	/*----------------
+	 * Object methods
+	 */
+	
+	@Override
+	public abstract ParameterList3<Key> clone();
+	
+	/*------------------------
+	 * IParameterList methods
+	 */
+
+	@Override
+	public ParameterValue getParameterValue(int index)
+	{
+		switch (index)
+		{
+		case 0:
+			return _parameter0;
+		case 1:
+			return _parameter1;
+		case 2:
+			return _parameter2;
+		default:
+			throw indexOutOfRange(index);
+		}
+	}
+	
+	@Override
+	public void setParameterValue(int index, ParameterValue value)
+	{
+		switch (index)
+		{
+		case 0:
+			_parameter0 = value;
+			break;
+		case 1:
+			_parameter1 = value;
+			break;
+		case 2:
+			_parameter2 = value;
+			break;
+		default:
+			throw indexOutOfRange(index);
+		}
+	}
+	
+	@Override
+	public int size()
+	{
+		return 3;
+	}
+
+}
