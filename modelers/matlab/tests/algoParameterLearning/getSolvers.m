4f4ed5641e264504a4d8f1b586af23a5d22e89cf
Shawn Hershey <shawn.hershey@analog.com>
2013-07-02 21:46:35 -0400

    added test stuff

--- modelers/matlab/tests/algoParameterLearning/getSolvers.m
+++ modelers/matlab/tests/algoParameterLearning/getSolvers.m
@@ -14,29 +14,9 @@
 %   limitations under the License.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%
-% This function uses the gibbs sampler to generate samples for a given 
-% graph
-function samples = collectSamples(fg,burnIns,numSamples,scansPerSamples)
-
-    BURN_INS = burnIns;
-    NUM_SAMPLES = numSamples;
-    SCANS_PER_SAMPLE = scansPerSamples;
-
-    fg.Solver = 'gibbs';
-    fg.Solver.setBurnInScans(BURN_INS);
-    fg.Solver.setNumSamples(NUM_SAMPLES);
-    fg.Solver.setScansPerSample(SCANS_PER_SAMPLE);
-    fg.Solver.saveAllSamples();
-    fg.Solver.setSeed(1);
-    fg.Solver.saveAllScores();
-    fg.solve();
-
-    numvars = length(fg.Variables);
-    samples = zeros(NUM_SAMPLES,numvars);
-
-    for i = 1:numvars
-        samples(:,i) = cell2mat(cell(fg.Variables{i}.Solver.getAllSamples()));
-    end
-    
+function solvers = getSolvers()
+    %disp('++algoSumProduct.getSolvers')
+    solvers = {};
+    solvers = appendCell(solvers, com.analog.lyric.dimple.solvers.sumproduct.Solver());
+    %disp('--algoSumProduct.getSolvers')
 end
--- modelers/matlab/tests/algoParameterLearning/testPLsmallFactor.m
+++ modelers/matlab/tests/algoParameterLearning/testPLsmallFactor.m
@@ -1,27 +1,45 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Copyright 2013 Analog Devices, Inc.
+%
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function testPLsmallFactor()
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Create single factor and vars
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    diffs = [1e-8 1e-3 1e-2];
+    verbose = 0;
+    
+    %provide different tolerances for the differnet degrees
+    diffs = [1e-8 1e-2 1e-2];
 
+    %Make things predictable
+    rand('seed',1);
+
+    %Sweep over different degrees of factor
     for B = 2:4
-
-        rand('seed',1);
-
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % First test simply checks to see that the parameters are learned
-        % correctly
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-        fg = FactorGraph();
-
         dims = ones(1,B)*2;
 
+        %Create the graph
+        fg = FactorGraph();
         b = Bit(B,1);
         weights = rand(dims);
         weights = weights / sum(weights(:));
         fg.addFactor(weights,b);
 
+        %Generate samples
         N = 1000;
-
         fg.Solver = 'gibbs';
         fg.Solver.setBurnInScans(10);
         fg.Solver.setNumSamples(N);
@@ -30,13 +48,12 @@ function testPLsmallFactor()
         fg.Solver.setSeed(1);
         fg.Solver.saveAllScores();
         fg.solve();
-
         samples = zeros(N,B);
-
         for i = 1:B
            samples(:,i) =  cell2mat(cell(b(i).Solver.getAllSamples()));
         end
 
+        %Calculate imperical weights
         imperical_weights = zeros(dims);
 
         for i = 1:size(samples,1)
@@ -47,21 +64,52 @@ function testPLsmallFactor()
 
         imperical_weights = imperical_weights / sum(imperical_weights(:));
 
+        %%%%%%%%%%%%%%%%%%%%%%%
+        %Learn the parameters
+        
+        %Build the factor graph
         fg2 = FactorGraph();
         b2 = Bit(B,1);
         f = fg2.addFactor(rand(dims),b2);
 
+        %Create the learner
         pl = PLLearner(fg2,{f.FactorTable},{b2});
 
+        %%%%%%%%%%%%%%%%%
+        % Compare gradient to numerical gradient
+        pl.setData(samples);
+        gradient = pl.calculateGradient();
+        num_gradient = zeros(1,2^B);
+        delta = 0.00001;
+        for i = 1:length(num_gradient)
+            num_gradient(i) = pl.calculateNumericalGradient(f.FactorTable,i,delta);
+        end
+        diff = num_gradient-gradient;
+        l2 = norm(diff);
+        
+        if verbose
+            fprintf('l2 of numerical gradient: %f\n',l2);
+        end
+        assertTrue(l2 < 1e-5);
+        
+        %Learn
         numSteps = 2000;
         scaleFactor = 0.05;
+        if verbose
+            disp('learning...');
+        end
         pl.learn(numSteps,samples,scaleFactor);
 
+        %Compare the weights
         cdims = num2cell(dims);
         learned_weights = reshape(f.FactorTable.Weights,cdims{:});
 
-        diff = learned_weights-imperical_weights;
+        diff = learned_weights-imperical_weights;        
         l2 = norm(diff(:));
+        
+        if verbose
+            fprintf('l2: %f\n',l2);
+        end
         
         assertTrue(l2 < diffs(B-1));
     end
