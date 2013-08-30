8b32cd6020ae407cc24d13f69c5f87b9bf045776
Christopher Barber <Christopher.Barber@analog.com>
2013-08-30 09:28:26 -0400

    Refactor FactorTablePerformanceTester to use Runnable for test cases

--- solvers/java/src/test/java/com/analog/lyric/dimple/test/FactorFunctions/core/FactorTablePerformanceTester.java
+++ solvers/java/src/test/java/com/analog/lyric/dimple/test/FactorFunctions/core/FactorTablePerformanceTester.java
@@ -16,6 +16,10 @@ public class FactorTablePerformanceTester
 	private final Random _random;
 	public boolean showLog = true;
 	
+	/*---------------
+	 * Construction
+	 */
+	
 	FactorTablePerformanceTester(IFactorTable table, int iterations)
 	{
 		_table = table;
@@ -24,151 +28,139 @@ public class FactorTablePerformanceTester
 		_timer = new Stopwatch();
 	}
 	
+	/*------------
+	 * Test cases
+	 */
+	
 	public double testEvalAsFactorFunction()
 	{
 		_random.setSeed(23);
-		Object [][] argrows = new Object[_iterations][];
+		final Object [][] argrows = new Object[_iterations][];
 		for (int i = 0; i < _iterations; ++i)
 		{
 			argrows[i] = randomArguments();
 		}
 
-		// warmup
-		for (Object[] args : argrows)
-		{
-			_table.evalAsFactorFunction(args);
-		}
-
-		_timer.reset();
-		_timer.start();
-
-		for (Object[] args : argrows)
-		{
-			_table.evalAsFactorFunction(args);
-		}
-
-		 _timer.stop();
-		 long ns = _timer.elapsed(TimeUnit.NANOSECONDS);
-		 return logTime("evalAsFactorFunction", ns / _iterations, "call");
+		Runnable test = new Runnable() {
+			@Override
+			public void run()
+			{
+				for (Object[] args : argrows)
+				{
+					_table.evalAsFactorFunction(args);
+				}
+			}
+		};
+		
+		return runTest("evalAsFactorFunction", test);
 	}
 	
 	public double testGet()
 	{
 		_random.setSeed(42);
-		int [][] rows = new int[_iterations][];
+		final int [][] rows = new int[_iterations][];
 		for (int i = 0; i < _iterations; ++i)
 		{
 			rows[i] = randomIndices();
 		}
 		
-		// warmup
-		for (int[] indices : rows)
-		{
-			_table.get(indices);
-		}
-
-		_timer.reset();
-		_timer.start();
-
-		for (int[] indices : rows)
-		{
-			_table.get(indices);
-		}
-
-		_timer.stop();
-		 long ns = _timer.elapsed(TimeUnit.NANOSECONDS);
-		 return logTime("get", ns / _iterations, "call");
+		Runnable test = new Runnable() {
+			@Override
+			public void run()
+			{
+				for (int[] indices : rows)
+				{
+					_table.get(indices);
+				}
+			}
+		};
+		
+		return runTest("get", test);
 	}
 
 	public double testGetWeightIndexFromTableIndices()
 	{
 		_random.setSeed(42);
-		int [][] rows = new int[_iterations][];
+		final int [][] rows = new int[_iterations][];
 		for (int i = 0; i < _iterations; ++i)
 		{
 			rows[i] = randomIndices();
 		}
 		
-		// warmup
-		for (int[] indices : rows)
-		{
-			_table.getWeightIndexFromTableIndices(indices);
-		}
-
-		 _timer.reset();
-		 _timer.start();
-		 
-		 for (int[] indices : rows)
-		 {
-			 _table.getWeightIndexFromTableIndices(indices);
-		 }
-		 
-		 _timer.stop();
-		 long ns = _timer.elapsed(TimeUnit.NANOSECONDS);
-		 return logTime("getWeightIndexFromTableIndices", ns / _iterations, "call");
+		Runnable test = new Runnable() {
+			@Override
+			public void run()
+			{
+				for (int[] indices : rows)
+				{
+					_table.getWeightIndexFromTableIndices(indices);
+				}
+			}
+		};
+		
+		 return runTest("getWeightIndexFromTableIndices", test);
 	}
 	
 	public double testGetWeightForIndices()
 	{
 		_random.setSeed(43);
-		int [][] rows = new int[500][];
+		final int [][] rows = new int[500][];
 		for (int i = 0; i < rows.length; ++i)
 		{
 			rows[i] = randomIndices();
 		}
 
-		@SuppressWarnings("unused")
-		double total = 0.0;
-		
-		// warmup
-		final NewFactorTable newTable = getNewTable();
-		if (newTable != null)
-		{
-			for (int[] indices : rows)
-			{
-				total += newTable.getWeightForIndices(indices);
-			}
-		}
-		else
-		{
-			double[] weights = _table.getWeights();
-			for (int[] indices : rows)
-			{
-				total += weights[_table.getWeightIndexFromTableIndices(indices)];
-			}
-		}
-		
-		total = 0.0;
-		 _timer.reset();
-		 if (newTable != null)
-		 {
-			 _timer.start();
-			 for (int i = _iterations; --i>=0;)
-			 {
-				 for (int[] indices : rows)
-				 {
-					 total += newTable.getWeightForIndicesDense(indices);
-				 }
-			 }
-		 }
-		 else
-		 {
-			 _timer.start();
-			 for (int i = _iterations; --i>=0;)
-			 {
-				 double[] weights = _table.getWeights();
-				 for (int[] indices : rows)
-				 {
-					 total += weights[_table.getWeightIndexFromTableIndices(indices)];
-				 }
-			 }
-		 }
-		 
-		 _timer.stop();
-		 long ns = _timer.elapsed(TimeUnit.NANOSECONDS);
-		 return logTime("getWeightForIndices", ns / (_iterations * rows.length), "call");
-	}
+		Runnable test = new Runnable() {
+			double total = 0.0;
 
+			@Override
+			public void run()
+			{
+				final NewFactorTable newTable = getNewTable();
+				
+				if (newTable != null)
+				{
+					runNewTable(newTable);
+				}
+				else
+				{
+					runOldTable(_table);
+				}
+			}
+			
+			private void runNewTable(NewFactorTable newTable)
+			{
+				for (int i = _iterations; --i>=0;)
+				{
+					for (int[] indices : rows)
+					{
+//						int ji = newTable.getDomainIndexer().jointIndexFromIndices(indices);
+//						total += newTable.getWeightForJointIndex(ji);
+						total += newTable.getWeightForIndices(indices);
+					}
+				}
+			}
+
+			private void runOldTable(IFactorTable table)
+			{
+				for (int i = _iterations; --i>=0;)
+				{
+					double[] weights = table.getWeights();
+					for (int[] indices : rows)
+					{
+						total += weights[table.getWeightIndexFromTableIndices(indices)];
+					}
+				}
+			}
+		};
+		
+		 return runTest("getWeightForIndices", rows.length, "call", 42, test);
+	}
+	
+	/*-----------------
+	 * Private methods
+	 */
+	
 	private NewFactorTable getNewTable()
 	{
 		if (_table instanceof NewFactorTable)
@@ -200,6 +192,33 @@ public class FactorTablePerformanceTester
 			indices[i] = _random.nextInt(domains[i].size());
 		}
 		return indices;
+	}
+	
+	private double runTest(String name, int unitMultiplier, String unit, int seed, Runnable test)
+	{
+		_random.setSeed(seed);
+		
+		// Warmup
+		test.run();
+		
+		_random.setSeed(seed);
+		_timer.reset();
+		_timer.start();
+		test.run();
+		_timer.stop();
+		
+		 long ns = _timer.elapsed(TimeUnit.NANOSECONDS);
+		 return logTime(name, ns / (_iterations * unitMultiplier), unit);
+	}
+	
+	private double runTest(String name, String unit, int seed, Runnable test)
+	{
+		return runTest(name, 1, unit, seed, test);
+	}
+
+	private double runTest(String name, Runnable test)
+	{
+		return runTest(name, "call", 42, test);
 	}
 	
 	private double logTime(String name, double time, String unit)
