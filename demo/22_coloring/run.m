25ddf852b31b85ad69a02cdae8ded3160493986f
Theophane Weber <theophane@gmail.com>
2013-09-17 23:36:07 -0400

    Some documentation fixes (bethe free energy, LP solver API)

--- demo/22_coloring/run.m
+++ demo/22_coloring/run.m
@@ -1,6 +1,16 @@
-num_countries=10;
-num_max_colors=10;
-[Adj_matrix,Vertex_info,C_info]=voronoi_box(num_countries); % fails sometimes - rarely
+
+num_max_colors=10; %upper bound on the number of colors required to color the graph
+num_countries=10; % number of countries
+
+load graph.mat; %contains information about a graph with ten polygons (or countries)
+
+% Adj_matrix contains information about the adjacency of different
+% countries - Adj_matrix(i,j) is 1 if and only if i and j share a border
+% Vertex_info contains the location of the vertices of the polygon
+% For each country i, C_info{i} contains the list of its corners as
+% vertices index (where the indices are consistent with the rows of
+% Vertex_info
+
 
 setSolver('lp');
 
@@ -8,24 +18,24 @@ setSolver('lp');
 fg=FactorGraph();
 
 
-ColorMax=Variable(1:num_max_colors,1,1);
-Colors=Variable(1:num_max_colors,num_countries,1);
+ColorMax=Variable(1:num_max_colors,1,1); %maximum number of colors to be used
+Colors=Variable(1:num_max_colors,num_countries,1); % color of each country
 not_equal=@(x,y) (x~=y);
 less_than=@(x,y) (x<=y);
 
 for i=1:num_countries-1
     for j=(i+1):num_countries
         if Adj_matrix(i,j)
-            fg.addFactor(not_equal,Colors(i),Colors(j));
+            fg.addFactor(not_equal,Colors(i),Colors(j)); % two neighboring countries cannot use the same color.
         end
     end
 end
 
 for i=1:num_countries
-    fg.addFactor(less_than,Colors(i),ColorMax);
+    fg.addFactor(less_than,Colors(i),ColorMax); % The ith color can only be used if at least i colors are used.
 end
 
-ColorMax.Input=exp(-(1:10));
+ColorMax.Input=exp(-(1:10)); %minimize the number of colors (L0 norm)
 
 
 
