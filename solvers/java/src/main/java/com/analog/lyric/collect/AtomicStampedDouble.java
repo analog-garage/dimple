830dea5a4acaef6345d2cf9a143eb127dcaed84b
Christopher Barber <Christopher.Barber@analog.com>
2013-10-08 12:02:59 -0400

    Added AtomicStampedDouble utility class

---
+++ solvers/java/src/main/java/com/analog/lyric/collect/AtomicStampedDouble.java
@@ -0,0 +1,178 @@
+package com.analog.lyric.collect;
+
+import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+import net.jcip.annotations.Immutable;
+import net.jcip.annotations.ThreadSafe;
+
+/**
+ * Maintains a double value along with an integer "stamp" that can be updated atomically.
+ * <p>
+ * This is a slightly more efficient alternative to {@code AtomicStampedReference<Double>}
+ * and unlike that class is serializable.
+ * 
+ * @see AtomicStampedReference
+ */
+@ThreadSafe
+public class AtomicStampedDouble implements Serializable
+{
+	/*-------
+	 * State
+	 */
+	
+	private static final long serialVersionUID = 1L;
+
+	@Immutable
+	protected static class Pair implements Serializable
+	{
+		private static final long serialVersionUID = 1L;
+
+		private final double _value;
+		private final int _stamp;
+		
+		private Pair(double value, int stamp)
+		{
+			_value = value;
+			_stamp = stamp;
+		}
+	}
+	
+	/**
+	 * Holds the current value/stamp pair.
+	 */
+	protected final AtomicReference<Pair> _atomicRef;
+	
+	/*--------------
+	 * Construction
+	 */
+	
+	/**
+	 * Constructs with given value and stamp.
+	 */
+	public AtomicStampedDouble(double value, int stamp)
+	{
+		_atomicRef = new AtomicReference<Pair>(new Pair(value, stamp));
+	}
+	
+	/**
+	 * Constructs with given value and stamp set to zero.
+	 */
+	public AtomicStampedDouble(double value)
+	{
+		this(value, 0);
+	}
+	
+	/**
+	 * Constructs with value and stamp both set to zero.
+	 */
+	public AtomicStampedDouble()
+	{
+		this(0,0);
+	}
+	
+	/**
+	 * Copies value and stamp from another instance.
+	 */
+	public AtomicStampedDouble(AtomicStampedDouble that)
+	{
+		_atomicRef = new AtomicReference<Pair>(that._atomicRef.get());
+	}
+	
+	/*---------
+	 * Methods
+	 */
+	
+	/**
+	 * Attempts to change the stamp if value matches {@code expectedValue}.
+	 * 
+	 * @return false if current value does not match {@code expectedValue} or if value
+	 * or stamp is changed in another thread during this call.
+	 * <p>
+	 * @see #compareAndSet
+	 */
+	public final boolean attemptStamp(double expectedValue, int newStamp)
+	{
+		Pair pair = _atomicRef.get();
+		return Double.doubleToRawLongBits(expectedValue) == Double.doubleToRawLongBits(pair._value) &&
+			(newStamp == pair._stamp ||	_atomicRef.compareAndSet(pair, new Pair(expectedValue, newStamp)));
+	}
+	
+	/**
+	 * Attempts to change the value and stamp atomically if current values match old values.
+	 * 
+	 * @return false if current value or stamp does not match expected values or if
+	 * value or stamp is changed in another thread during this call.
+	 * <p>
+	 * @see #attemptStamp
+	 * @see #setAndIncrementStamp(double)
+	 */
+	public final boolean compareAndSet(double expectedValue, double newValue, int expectedStamp, int newStamp)
+	{
+		Pair pair = _atomicRef.get();
+		return expectedStamp == pair._stamp &&
+			Double.doubleToRawLongBits(expectedValue) == Double.doubleToRawLongBits(pair._value) &&
+			((newValue == expectedValue && expectedStamp == newStamp) ||
+				_atomicRef.compareAndSet(pair, new Pair(newValue, newStamp)));
+	}
+	
+	/**
+	 * Gets the current value.
+	 */
+	public final double get()
+	{
+		return _atomicRef.get()._value;
+	}
+
+	/**
+	 * Returns the current value and writes into {@code stampHolder} the current stamp value.
+	 * @param stampHolder must have lenght of at least one.
+	 */
+	public final double get(int[] stampHolder)
+	{
+		Pair pair = _atomicRef.get();
+		stampHolder[0] = pair._stamp;
+		return pair._value;
+	}
+	
+	/**
+	 * Returns the current stamp value.
+	 */
+	public final int getStamp()
+	{
+		return _atomicRef.get()._stamp;
+	}
+	
+	/**
+	 * Atomically sets the value and its associated stamp.
+	 */
+	public final void set(double value, int stamp)
+	{
+		Pair pair = _atomicRef.get();
+		if (pair._value != value || pair._stamp != stamp)
+		{
+			_atomicRef.set(new Pair(value, stamp));
+		}
+	}
+	
+	/**
+	 * Sets the value and atomically increments the stamp if the value changed.
+	 * 
+	 * @return false if the value was not set because it was set by another thread
+	 * 
+	 * @see #compareAndSet
+	 */
+	public final boolean setAndIncrementStamp(double value)
+	{
+		Pair pair = _atomicRef.get();
+		
+		if (Double.doubleToRawLongBits(value) != Double.doubleToRawLongBits(pair._value))
+		{
+			return _atomicRef.compareAndSet(pair, new Pair(value, pair._stamp + 1));
+		}
+		
+		return true;
+	}
+	
+}
---
+++ solvers/java/src/test/java/com/analog/lyric/collect/tests/TestAtomicStampedDouble.java
@@ -0,0 +1,167 @@
+package com.analog.lyric.collect.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.Test;
+
+import com.analog.lyric.collect.AtomicStampedDouble;
+import com.analog.lyric.util.test.SerializationTester;
+
+public class TestAtomicStampedDouble
+{
+
+	@Test
+	public void test()
+	{
+		int[] stampHolder = new int[1];
+		
+		//
+		// Single threaded tests
+		//
+		
+		// Construction
+		AtomicStampedDouble d1 = new AtomicStampedDouble();
+		assertEquals(0.0, d1.get(), 0.0);
+		assertEquals(0, d1.getStamp());
+		stampHolder[0] = -1;
+		assertEquals(0.0, d1.get(stampHolder), 0.0);
+		assertEquals(0, stampHolder[0]);
+		
+		d1 = new AtomicStampedDouble(3.145);
+		assertEquals(3.145, d1.get(), 0.0);
+		assertEquals(0, d1.getStamp());
+		stampHolder[0] = -1;
+		assertEquals(3.145, d1.get(stampHolder), 0.0);
+		assertEquals(0, stampHolder[0]);
+	
+		d1 = new AtomicStampedDouble(42, 23);
+		assertEquals(42, d1.get(), 0.0);
+		assertEquals(23, d1.getStamp());
+		stampHolder[0] = -1;
+		assertEquals(42, d1.get(stampHolder), 0.0);
+		assertEquals(23, stampHolder[0]);
+		
+		// Copying
+		AtomicStampedDouble d2 = new AtomicStampedDouble(d1);
+		assertEquals(d1.get(), d2.get(), 0.0);
+		assertEquals(d1.getStamp(), d2.getStamp());
+		
+		// Serialization
+		d2 = SerializationTester.clone(d1);
+		assertEquals(d1.get(), d2.get(), 0.0);
+		assertEquals(d1.getStamp(), d2.getStamp());
+		
+		// set
+		d1.set(1.2, 2);
+		assertEquals(1.2, d1.get(), 0.0);
+		assertEquals(2, d1.getStamp());
+		d1.set(1.2, 2);
+		assertEquals(1.2, d1.get(), 0.0);
+		assertEquals(2, d1.getStamp());
+		d1.set(1.2, 3);
+		assertEquals(1.2, d1.get(), 0.0);
+		assertEquals(3, d1.getStamp());
+		d1.set(1.3, 3);
+		assertEquals(1.3, d1.get(), 0.0);
+		assertEquals(3, d1.getStamp());
+		
+		// setAndIncrementStamp
+		d1.setAndIncrementStamp(123);
+		assertEquals(123, d1.get(), 0.0);
+		assertEquals(4, d1.getStamp());
+		
+		// attemptStamp
+		assertFalse(d1.attemptStamp(42, 9));
+		assertEquals(123, d1.get(), 0.0);
+		assertEquals(4, d1.getStamp());
+		assertTrue(d1.attemptStamp(123, 9));
+		assertEquals(123, d1.get(), 0.0);
+		assertEquals(9, d1.getStamp());
+		
+		// compareAndSet
+		assertFalse(d1.compareAndSet(1.23, 1.45, 9, 11));
+		assertEquals(123, d1.get(), 0.0);
+		assertEquals(9, d1.getStamp());
+		assertFalse(d1.compareAndSet(123, 1.45, 3, 11));
+		assertEquals(123, d1.get(), 0.0);
+		assertEquals(9, d1.getStamp());
+		assertTrue(d1.compareAndSet(123, 1.45, 9, 11));
+		assertEquals(1.45, d1.get(), 0.0);
+		assertEquals(11, d1.getStamp());
+		
+		//
+		// Multithreaded tests
+		//
+		
+		final int nThreads = Runtime.getRuntime().availableProcessors();
+		int iterations = 10000;
+		CountDownLatch startRandomSet = new CountDownLatch(1);
+		RandomSetTester[] setTesters = new RandomSetTester[nThreads];
+		for (int i = 0; i < nThreads; ++i)
+		{
+			setTesters[i] = new RandomSetTester(d1, i * iterations, iterations, startRandomSet);
+			setTesters[i].start();
+		}
+		
+		startRandomSet.countDown(); // Wave the start flag for the test threads
+		
+		for (int i = 0; i < nThreads; ++i)
+		{
+			try
+			{
+				setTesters[i].join();
+			}
+			catch (InterruptedException ex)
+			{
+				// TODO Auto-generated catch block
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	private static class RandomSetTester extends Thread
+	{
+		private final AtomicStampedDouble _d;
+		private final int _iterations;
+		private final CountDownLatch _startLatch;
+		private final int _startValue;
+		
+		RandomSetTester(AtomicStampedDouble d, int value, int iterations, CountDownLatch startLatch)
+		{
+			_d = d;
+			_iterations = iterations;
+			_startLatch = startLatch;
+			_startValue = value;
+		}
+		
+		@Override
+		public void run()
+		{
+			try
+			{
+				_startLatch.await();
+			}
+			catch (InterruptedException ex)
+			{
+				// TODO Auto-generated catch block
+				ex.printStackTrace();
+			}
+			
+			int[] stampHolder = new int[1];
+	
+			for (int i = _iterations, val = _startValue; --i>=0; ++val)
+			{
+				_d.set(val, val);
+				double val2 = _d.get(stampHolder);
+				assertEquals((int)val2, stampHolder[0]);
+				
+				_d.compareAndSet(val, val, val-1, val-1);
+				
+				val2 = _d.get(stampHolder);
+				assertEquals((int)val2, stampHolder[0]);
+			}
+		}
+	}
+}
