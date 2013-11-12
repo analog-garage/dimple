c755cc8c889f1cbdb1bd482edac243d59f26d7c4
Shawn Hershey <shawn.hershey@analog.com>
2013-11-12 17:06:38 -0500

    updated longver.py

--- solvers/java/longver.py
+++ solvers/java/longver.py
@@ -29,9 +29,9 @@ h = hash(s)
 nouns = file('nouns.txt','r').readlines()
 adjs = file('adj.txt','r').readlines()
 advs = file('adv.txt','r').readlines()
-s +=     ' "' + advs[h % len(advs)].strip().lower().capitalize() + \
-         ' ' + adjs[h % len(adjs)].strip().lower().capitalize() + \
-         ' ' + nouns[h % len(nouns)].strip().lower().capitalize() + '"'
+s +=     ' "' + advs[h % len(advs)].strip().capitalize() + \
+         ' ' + adjs[h % len(adjs)].strip().capitalize() + \
+         ' ' + nouns[h % len(nouns)].strip() + '"'
 f.write(s)
 f.close()
 shutil.copy('src/main/resources/VERSION','../../LONG_VERSION')
