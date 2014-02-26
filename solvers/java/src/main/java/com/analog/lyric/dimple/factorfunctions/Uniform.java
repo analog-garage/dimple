556bfcd7dd1279528451d18f70809bf025d2d3fb
Christopher Barber <Christopher.Barber@analog.com>
2014-02-26 16:31:08 -0500

    Added Uniform factor function

---
+++ solvers/java/src/main/java/com/analog/lyric/dimple/factorfunctions/Uniform.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+*   Copyright 2014 Analog Devices, Inc.
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+********************************************************************************/
+
+package com.analog.lyric.dimple.factorfunctions;
+
+import com.analog.lyric.dimple.factorfunctions.core.FactorFunction;
+import com.analog.lyric.dimple.model.values.Value;
+
+/**
+ * Uniform distribution. Returns zero energy for all combination of arguments.
+ * 
+ * @since 0.05
+ */
+public class Uniform extends FactorFunction
+{
+	public static Uniform INSTANCE = new Uniform();
+	
+	public Uniform()
+	{
+		super("Uniform");
+	}
+	
+	@Override
+	public double eval(Object ... arguments)
+	{
+		return 1.0;
+	}
+	
+	@Override
+	public double evalEnergy(Object ... arguments)
+	{
+		return 0.0;
+	}
+
+	@Override
+	public double evalEnergy(Value[] values)
+	{
+		return 0.0;
+	}
+}
