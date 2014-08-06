0f62a8696b0721f3d142dbb30bb751500925ea1a
Christopher Barber <Christopher.Barber@analog.com>
2014-08-06 14:49:31 -0400

    Added DimpleEnvironment class

---
+++ solvers/java/src/main/java/com/analog/lyric/dimple/environment/DimpleEnvironment.java
@@ -0,0 +1,256 @@
+/*******************************************************************************
+*   Copyright 2014 Analog Devices, Inc.
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+********************************************************************************/
+
+package com.analog.lyric.dimple.environment;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.logging.Logger;
+
+import net.jcip.annotations.ThreadSafe;
+
+import com.analog.lyric.dimple.events.IDimpleEventListener;
+import com.analog.lyric.dimple.events.IDimpleEventSource;
+import com.analog.lyric.dimple.events.IModelEventSource;
+import com.analog.lyric.dimple.model.core.FactorGraph;
+import com.analog.lyric.dimple.options.DimpleOptionHolder;
+import com.analog.lyric.util.misc.Nullable;
+
+/**
+ * 
+ * @since 0.07
+ * @author Christopher Barber
+ */
+@ThreadSafe
+public class DimpleEnvironment extends DimpleOptionHolder
+{
+	/*--------------
+	 * Static state
+	 */
+	
+	private static final AtomicReference<DimpleEnvironment> _globalInstance =
+		new AtomicReference<>(new DimpleEnvironment());
+	
+	private static final ThreadLocal<DimpleEnvironment> _threadInstance = new ThreadLocal<DimpleEnvironment>() {
+		@Override
+		protected DimpleEnvironment initialValue()
+		{
+			return global();
+		}
+	};
+	
+	/*----------------
+	 * Instance state
+	 */
+	
+	// NOTE: although the environment is typically accessed through a thread-local
+	// variable, there will typically be only one environment shared across all threads
+	// so care should be taken to ensure that the code is thread safe.
+	
+	/**
+	 * Logging instance for this environment.
+	 */
+	private final AtomicReference<Logger> _logger = new AtomicReference<>();
+	
+	/*--------------
+	 * Construction
+	 */
+	
+	/**
+	 * Constructs a new instance of a Dimple environment.
+	 * <p>
+	 * @since 0.07
+	 */
+	public DimpleEnvironment()
+	{
+		final Logger logger = Logger.getLogger("com.analog.lyric.dimple");
+		_logger.set(logger);
+	}
+	
+	/*----------------
+	 * Static methods
+	 */
+	
+	/**
+	 * The default global instance of the dimple environment.
+	 * <p>
+	 * This is used as the initial value of the {@link #local} environment
+	 * for newly created threads. Most users should use {@link #local} instead of this.
+	 * <p>
+	 * @see #setGlobal(DimpleEnvironment)
+	 * @since 0.07
+	 */
+	public static DimpleEnvironment global()
+	{
+		return _globalInstance.get();
+	}
+	
+	/**
+	 * The thread-specific instance of the dimple environment.
+	 * <p>
+	 * This is initialized to be the same as the {@link #global} instance the
+	 * first time this is invoked, but can be overridden. Users should remember
+	 * that modifications to this environment will affect other threads unless
+	 * the environment has been set to a value unique to the current thread.
+	 * <p>
+	 * @see #setLocal(DimpleEnvironment)
+	 * @since 0.07
+	 */
+	public static DimpleEnvironment local()
+	{
+		return _threadInstance.get();
+	}
+	
+	/**
+	 * Sets the global dimple environment to a new instance.
+	 * @param env is a non-null environment instance.
+	 * @since 0.07
+	 */
+	public static void setGlobal(DimpleEnvironment env)
+	{
+		_globalInstance.set(env);
+	}
+	
+	/**
+	 * Sets the thread-specific dimple environment to a new instance.
+	 * @param env is a non-null environment instance.
+	 * @since 0.07
+	 */
+	public static void setLocal(DimpleEnvironment env)
+	{
+		_threadInstance.set(env);
+	}
+	
+	/*----------------------------
+	 * IDimpleEventSource methods
+	 */
+
+	@Override
+	public @Nullable FactorGraph getContainingGraph()
+	{
+		return null;
+	}
+
+	@Override
+	public @Nullable IDimpleEventListener getEventListener()
+	{
+		// TODO: move event listener here from FactorGraph
+		return null;
+	}
+
+	@Override
+	public @Nullable IDimpleEventSource getEventParent()
+	{
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * <p>
+	 * This implementation returns the string {@code "DimpleEnvironment"}.
+	 */
+	@Override
+	public String getEventSourceName()
+	{
+		return "DimpleEnvironment";
+	}
+
+	@Override
+	public @Nullable IModelEventSource getModelEventSource()
+	{
+		return null;
+	}
+
+	@Override
+	public void notifyListenerChanged()
+	{
+	}
+	
+	/*---------------------------
+	 * DimpleEnvironment methods
+	 */
+	
+	/**
+	 * Logs a warning message using the thread-specific Dimple logger.
+	 * <p>
+	 * This is simply shorthand for:
+	 * <blockquote>
+     *   local().logger().warning(String.format(format, args));
+	 * </blockquote>
+	 * <p>
+	 * @param format a non-null String for use with {@link String#format}.
+	 * @param args zero or more format arguments.
+	 * @since 0.07
+	 */
+	public static void logWarning(String format, Object ... args)
+	{
+		local().logger().warning(String.format(format, args));
+	}
+	
+	/**
+	 * Logs an error message using the thread-specific Dimple logger.
+	 * <p>
+	 * This is simply shorthand for:
+	 * <blockquote>
+     *   local().logger().severe(String.format(format, args));
+	 * </blockquote>
+	 * <p>
+	 * @param format a non-null String for use with {@link String#format}.
+	 * @param args zero or more format arguments.
+	 * @since 0.07
+	 */
+	public static void logSevere(String format, Object ... args)
+	{
+		local().logger().severe(String.format(format, args));
+	}
+	
+	/**
+	 * The environment-specific logger instance.
+	 * <p>
+	 * By default this is obtained by invoking
+	 * {@linkplain Logger#getLogger(String) Logger.getLogger("com.analog.lyric.dimple")}
+	 * when the environment is first initialized.
+	 * <p>
+	 * Logging is typically configured using Java system properties, which is described
+	 * in the documentation for {@link java.util.logging.LogManager}. You can also configure
+	 * the logger object directly.
+	 * <p>
+	 * @see #setLogger
+	 * @see #logWarning
+	 * @see #logSevere
+	 * @see java.util.logging.Logger
+	 * @see java.util.logging.LogManager
+	 * @since 0.07
+	 */
+	public Logger logger()
+	{
+		return _logger.get();
+	}
+	
+	/**
+	 * Sets the environment-specific {@link #logger} instance.
+	 * <p>
+	 * This can be used to replace the logger with special implementation
+	 * for test purposes when testing logging behavior.
+	 * <p>
+	 * @param logger
+	 * @return the previous logger instance.
+	 * @since 0.07
+	 */
+	public Logger setLogger(Logger logger)
+	{
+		return _logger.getAndSet(logger);
+	}
+}
---
+++ solvers/java/src/test/java/com/analog/lyric/dimple/test/environment/TestDimpleEnvironment.java
@@ -0,0 +1,130 @@
+/*******************************************************************************
+*   Copyright 2014 Analog Devices, Inc.
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+********************************************************************************/
+
+package com.analog.lyric.dimple.test.environment;
+
+import static org.junit.Assert.*;
+
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import org.junit.Test;
+
+import com.analog.lyric.dimple.environment.DimpleEnvironment;
+import com.analog.lyric.util.test.TestLogger;
+
+/**
+ * Unit tests for {@link DimpleEnvironment} class.
+ * @since 0.07
+ * @author Christopher Barber
+ */
+public class TestDimpleEnvironment
+{
+	@Test
+	public void testInstances()
+	{
+		final DimpleEnvironment global1 = DimpleEnvironment.global();
+		final DimpleEnvironment local1 = DimpleEnvironment.local();
+		assertSame(global1, local1);
+		
+		Thread thread1 = new Thread() {
+			@Override
+			public void run()
+			{
+				assertSame(global1, DimpleEnvironment.local());
+				DimpleEnvironment local2 = new DimpleEnvironment();
+				DimpleEnvironment.setLocal(local2);
+				assertSame(local2, DimpleEnvironment.local());
+			}
+		};
+		thread1.start();
+		try
+		{
+			thread1.join();
+		}
+		catch (InterruptedException ex)
+		{
+			fail(ex.toString());
+		}
+		
+		assertSame(local1, DimpleEnvironment.local());
+		
+		final DimpleEnvironment global2 = new DimpleEnvironment();
+		DimpleEnvironment.setGlobal(global2);
+		assertSame(global2, DimpleEnvironment.global());
+		assertSame(local1, DimpleEnvironment.local());
+		
+		Thread thread2 = new Thread() {
+			@Override
+			public void run()
+			{
+				assertSame(global2, DimpleEnvironment.local());
+				DimpleEnvironment.setGlobal(global1);
+				assertSame(global1, DimpleEnvironment.global());
+				assertSame(global2, DimpleEnvironment.local());
+			}
+		};
+		thread2.start();
+		try
+		{
+			thread2.join();
+		}
+		catch (InterruptedException ex)
+		{
+			fail(ex.toString());
+		}
+	}
+	
+	@Test
+	public void testEventSourceMethods()
+	{
+		DimpleEnvironment global = DimpleEnvironment.global();
+		assertNull(global.getContainingGraph());
+		assertNull(global.getEventParent());
+		assertNull(global.getModelEventSource());
+		global.notifyListenerChanged(); // does nothing
+
+		assertEquals("DimpleEnvironment", global.getEventSourceName());
+		
+		assertNull(global.getEventListener());
+	}
+	
+	@Test
+	public void testLogging()
+	{
+		DimpleEnvironment env = DimpleEnvironment.local();
+		
+		Logger defaultLogger = env.logger();
+		assertSame(defaultLogger, Logger.getLogger("com.analog.lyric.dimple"));
+		
+		TestLogger testLogger = new TestLogger("com.analog.lyric.dimple");
+		env.setLogger(testLogger);
+		assertSame(testLogger, env.logger());
+		
+		DimpleEnvironment.logWarning("'%s'","hi there");
+		LogRecord record = testLogger.loggedRecords().remove();
+		assertEquals(Level.WARNING, record.getLevel());
+		assertEquals("'hi there'", record.getMessage());
+		assertTrue(testLogger.loggedRecords().isEmpty());
+
+		DimpleEnvironment.logSevere("'%s'","whoops");
+		record = testLogger.loggedRecords().remove();
+		assertEquals(Level.SEVERE, record.getLevel());
+		assertEquals("'whoops'", record.getMessage());
+		assertTrue(testLogger.loggedRecords().isEmpty());
+	}
+}
