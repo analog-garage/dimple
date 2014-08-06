d253ecff4fb464615aa48d8b1d15dc0c0eeaddf8
Christopher Barber <Christopher.Barber@analog.com>
2014-08-05 21:43:43 -0400

    Added DimpleOptionRegistry and some option tests

---
+++ solvers/java/src/main/java/com/analog/lyric/dimple/options/DimpleOptionRegistry.java
@@ -0,0 +1,116 @@
+/*******************************************************************************
+*   Copyright 2014 Analog Devices, Inc.
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+********************************************************************************/
+
+package com.analog.lyric.dimple.options;
+
+import com.analog.lyric.dimple.exceptions.DimpleException;
+import com.analog.lyric.dimple.solvers.gibbs.GibbsOptions;
+import com.analog.lyric.dimple.solvers.junctiontree.JunctionTreeOptions;
+import com.analog.lyric.dimple.solvers.lp.LPOptions;
+import com.analog.lyric.dimple.solvers.minsum.MinSumOptions;
+import com.analog.lyric.dimple.solvers.particleBP.ParticleBPOptions;
+import com.analog.lyric.dimple.solvers.sumproduct.SumProductOptions;
+import com.analog.lyric.options.IOptionKey;
+import com.analog.lyric.options.OptionRegistry;
+import com.analog.lyric.util.misc.Nullable;
+
+/**
+ * Registry of option keys for known dimple options.
+ * <p>
+ * @since 0.07
+ * @author Christopher Barber
+ */
+public enum DimpleOptionRegistry
+{
+	INSTANCE;
+	
+	private final OptionRegistry _registry = new OptionRegistry();
+	
+	private DimpleOptionRegistry()
+	{
+		_registry.addFromClass(DimpleOptions.class);
+		_registry.addFromClass(SolverOptions.class);
+		
+		_registry.addFromClass(GibbsOptions.class);
+		_registry.addFromClass(JunctionTreeOptions.class);
+		_registry.addFromClass(LPOptions.class);
+		_registry.addFromClass(MinSumOptions.class);
+		_registry.addFromClass(ParticleBPOptions.class);
+		_registry.addFromClass(SumProductOptions.class);
+	}
+	
+	/**
+	 * Underlying option registry.
+	 * @since 0.07
+	 */
+	public static OptionRegistry getRegistry()
+	{
+		return INSTANCE._registry;
+	}
+	
+	/**
+	 * Returns key for given qualified name or null.
+	 * <p>
+	 * @see OptionRegistry#get(String)
+	 * @param name is a non-null string containing either the fully qualified name of the option
+	 * or the name qualified with just the option class name (e.g. "SolverOptions.iterations").
+	 * @return key for name or null if not found.
+	 * @since 0.07
+	 */
+	public static @Nullable IOptionKey<?> getKey(String name)
+	{
+		return INSTANCE._registry.get(name);
+	}
+	
+	/**
+	 * Returns key for given qualified name or throws an error.
+	 * <p>
+	 * @param keyOrName either a {@link IOptionKey} instance which will simply be returned or
+	 * a {@link String} compatible with {@link #getKey(String)}.
+	 * @throws DimpleException if key not found or input argument is not the correct type.
+	 * @throws IllegalArgumentException if {@code keyOrName} does not have the correct type.
+	 * @since 0.07
+	 */
+	public static IOptionKey<?> asKey(Object keyOrName)
+	{
+		IOptionKey<?> key;
+		
+		if (keyOrName instanceof String)
+		{
+			String name = (String)keyOrName;
+			key = getKey(name);
+			if (key == null)
+			{
+				throw new DimpleException("Unknown option key '%s'", name);
+			}
+			else
+			{
+				return key;
+			}
+		}
+		else if (keyOrName instanceof IOptionKey)
+		{
+			key = (IOptionKey<?>)keyOrName;
+		}
+		else
+		{
+			throw new IllegalArgumentException(
+				String.format("Expected String or IOptionKey instead of '%s'", keyOrName.getClass()));
+		}
+		
+		return key;
+	}
+}
---
+++ solvers/java/src/test/java/com/analog/lyric/dimple/test/options/TestDimpleOptions.java
@@ -0,0 +1,92 @@
+/*******************************************************************************
+*   Copyright 2014 Analog Devices, Inc.
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+********************************************************************************/
+
+package com.analog.lyric.dimple.test.options;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+import com.analog.lyric.dimple.options.DimpleOptionRegistry;
+import com.analog.lyric.dimple.options.DimpleOptions;
+import com.analog.lyric.options.IOptionKey;
+import com.analog.lyric.options.OptionKey;
+import com.analog.lyric.options.OptionKeys;
+import com.analog.lyric.options.OptionRegistry;
+
+/**
+ * 
+ * @since 0.07
+ * @author Christopher Barber
+ */
+public class TestDimpleOptions
+{
+	/**
+	 * Test contents of {@link DimpleOptionRegistry} and {@link DimpleOptions} classes.
+	 * <p>
+	 * Makes sure that options are not accidentally omitted because they have mis-matched
+	 * name or are not declared "public static final".
+	 * <p>
+	 * @since 0.07
+	 */
+	@Test
+	public void test() throws IllegalAccessException
+	{
+		OptionRegistry registry = DimpleOptionRegistry.getRegistry();
+		
+		Set<IOptionKey<?>> allKeys = new HashSet<IOptionKey<?>>();
+		for (IOptionKey<?> key : registry)
+		{
+			assertSame(key, DimpleOptionRegistry.getKey(OptionKey.canonicalName(key)));
+			assertSame(key, DimpleOptionRegistry.getKey(OptionKey.qualifiedName(key)));
+			assertTrue(allKeys.add(key));
+		}
+		
+		Set<IOptionKey<?>> allKeys2 = new HashSet<IOptionKey<?>>();
+		for (OptionKeys keys : registry.getOptionKeys())
+		{
+			assertTrue(keys.size() > 0);
+			for (IOptionKey<?> key : keys)
+			{
+				assertTrue(allKeys2.add(key));
+			}
+			
+			Class<?> declaringClass = keys.declaringClass();
+			for (Field field : declaringClass.getDeclaredFields())
+			{
+				if (IOptionKey.class.isAssignableFrom(field.getType()))
+				{
+					final int modifiers = field.getModifiers();
+					assertTrue(Modifier.isFinal(modifiers));
+					assertTrue(Modifier.isPublic(modifiers));
+					assertTrue(Modifier.isStatic(modifiers));
+					
+					IOptionKey<?> key = (IOptionKey<?>)field.get(declaringClass);
+					assertEquals(field.getName(), key.name());
+					assertSame(key, DimpleOptionRegistry.getKey(OptionKey.qualifiedName(key)));
+					assertSame(declaringClass, key.getDeclaringClass());
+				}
+			}
+		}
+		
+		assertEquals(allKeys, allKeys2);
+	}
+}
