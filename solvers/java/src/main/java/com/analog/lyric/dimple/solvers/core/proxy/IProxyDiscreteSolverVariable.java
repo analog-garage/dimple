3d1a2e86923d8d8c100ca0765228e260e17aaae4
Christopher Barber <Christopher.Barber@analog.com>
2014-09-10 21:10:31 -0400

    Added IProxy* interfaces

---
+++ solvers/java/src/main/java/com/analog/lyric/dimple/solvers/core/proxy/IProxyDiscreteSolverVariable.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+*   Copyright 2014 Analog Devices, Inc.
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+********************************************************************************/
+
+package com.analog.lyric.dimple.solvers.core.proxy;
+
+import com.analog.lyric.dimple.model.variables.Discrete;
+import com.analog.lyric.dimple.solvers.interfaces.IDiscreteSolverVariable;
+
+/**
+ * 
+ * @since 0.07
+ * @author Christopher Barber
+ */
+public interface IProxyDiscreteSolverVariable<Delegate extends IDiscreteSolverVariable>
+	extends IProxySolverVariable<Delegate>, IDiscreteSolverVariable
+{
+	@Override
+	public Discrete getModelObject();
+}
---
+++ solvers/java/src/main/java/com/analog/lyric/dimple/solvers/core/proxy/IProxySolverNode.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ *   Copyright 2014 Analog Devices, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ ********************************************************************************/
+
+package com.analog.lyric.dimple.solvers.core.proxy;
+
+import org.eclipse.jdt.annotation.Nullable;
+
+import com.analog.lyric.dimple.model.core.INode;
+import com.analog.lyric.dimple.solvers.interfaces.ISolverNode;
+
+/**
+ * @param <Delegate>
+ * @since 0.07
+ * @author Christopher Barber
+ */
+public interface IProxySolverNode<Delegate extends ISolverNode> extends ISolverNode
+{
+	@Override
+	public INode getModelObject();
+	
+	public abstract @Nullable Delegate getDelegate();
+}
