65f05d0d9eb2fe349b4bc0828a7f5d90c3859ea1
Christopher Barber <Christopher.Barber@analog.com>
2015-03-13 19:34:04 +0100

    Remove obsolete dimple developer documentation

--- doc/builddoc.py
+++ doc/builddoc.py
@@ -57,11 +57,8 @@ if __name__ == "__main__":
     parser.add_option("-u","--user_doc",action="store_true",default=False,dest="user_doc")
     parser.add_option("-j","--java_user_doc",action="store_true",default=False,dest="java_user_doc")
     parser.add_option("-m","--matlab_user_doc",action="store_true",default=False,dest="matlab_user_doc")
-    parser.add_option("-d","--devel_doc",action="store_true",default=False,dest="devel_doc")
     parser.add_option("-f","--user_doc_filename",dest="user_doc_filename",default="DimpleUserManual")
     parser.add_option("-F","--user_doc_dir",dest="user_doc_dir",default="DimpleUserManual")
-    parser.add_option("-r","--devel_doc_filename",dest="devel_doc_filename",default="DimpleDeveloperDocumentation")
-    parser.add_option("-R","--devel_doc_dir",dest="devel_doc_dir",default="DimpleDeveloperDocumentation")
     parser.add_option("-c","--clean",dest="clean",default=False,action="store_true")
                       
     (option,args) = parser.parse_args()
@@ -82,20 +79,12 @@ if __name__ == "__main__":
         for n in glob.glob("*/*.out"):
             os.remove(n)
 
-    # user_doc  devel_doc options
-    #    F          F              build both unless clean
-    #    F          T              build devel_doc
-    #    T          F              build user_doc
-    #    T          T              build both
-
     if len(sys.argv) == 1:
         build_java_user_doc = True
         build_matlab_user_doc = True
-        build_devel_doc = True
     else:
         build_java_user_doc = False
         build_matlab_user_doc = False
-        build_devel_doc = False
 
     if option.user_doc:
         build_java_user_doc = True
@@ -104,8 +93,6 @@ if __name__ == "__main__":
         build_java_user_doc = True
     if option.matlab_user_doc:
         build_matlab_user_doc = True
-    if option.devel_doc:
-        build_devel_doc = True
 
     versionString = extractVersionString()
  
@@ -121,9 +108,3 @@ if __name__ == "__main__":
             produceUserDoc(option.user_doc_filename,'formatlab',versionString)
         shutil.copyfile(option.user_doc_dir + '/' + option.user_doc_filename + ".pdf",
                         option.user_doc_filename + "_v" + versionString + "_MATLAB_API.pdf")
-    # Create developer doc
-    if build_devel_doc:
-        with cd(option.devel_doc_dir):
-            producedoc(option.devel_doc_filename,option)
-        shutil.copyfile(option.devel_doc_dir + '/' + option.devel_doc_filename + ".pdf",
-                        option.devel_doc_filename + "_v" + versionString + ".pdf")
