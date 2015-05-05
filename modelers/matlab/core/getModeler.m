c765896d34c0327d792c196b5e9e7a023645a11d
Christopher Barber <Christopher.Barber@analog.com>
2015-05-05 21:19:51 +0100

    Remove obsolete MATLAB ModelFactory

--- modelers/matlab/core/getModeler.m
+++ modelers/matlab/core/getModeler.m
@@ -17,7 +17,6 @@
 function modeler = getModeler()
     global g_dimpleModeler;
     if isempty(g_dimpleModeler)
-        %g_dimpleModeler = ModelFactory();
         g_dimpleModeler = com.analog.lyric.dimple.matlabproxy.ModelFactory();
         g_dimpleModeler.setSolver(getSolver());
     end
